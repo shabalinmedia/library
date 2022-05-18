59073f6ea1f80ca2091d9baaa73d9339b2d90a4b
Alex Shabalin <shabalin.media@gmail.com>
2022-05-19 00:09:09 +0600

    Added some JS.

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ function Book() {
   // the constructor...
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+const yesButton = document.querySelector("#yes-button");
+const noButton = document.querySelector("#no-button");
+const addButton = document.querySelector("#add");
+const cancelButton = document.querySelector("#cancel");
+
+const addToLibrary = (ev) => {
+  ev.preventDefault();
+  let toLibrary = {
+    title: document.querySelector(".title").value,
+    author: document.querySelector(".author").value,
+    pages: document.querySelector(".pages").value,
+    readStatus: "HZ",
+  };
+
+  myLibrary.push(toLibrary);
+  document.querySelector(".form").value = "";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#add").addEventListener("click", addToLibrary);
+});
+
+console.log(myLibrary);
